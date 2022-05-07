35422d3d8e08a21f21f6cd7b909d5cb097aac1db
Megan Medeiros <meganmedeiros@Megans-MBP.fios-router.home>
2022-05-07 02:18:44 -0400

    Fixed dependencey issue, added navbar component and social icons as well as formatting for home page and building nav components for home page as well

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,11 +9,13 @@ export default class Navbar extends Component {
     render() {
         return(
             <div className='navbar'>
-                <ul className='nav-list'>
-                    <li>Zachary Johnson</li>
-                    <li><SocialIcon url='https://www.linkedin.com/johnsonzm' target='_blank' /></li>
-                    <li><SocialIcon url='https://www.github.com/zachmma99' target='_blank' /></li>
-                </ul>
+                <div className='navbar-items'>
+                    <h5>Zachary Johnson</h5>
+                    <div className='social-icons'>
+                        <SocialIcon url='https://www.linkedin.com/in/johnsonzm' target='_blank' className='social-icon' style={{ height: 35, width: 35 }} />
+                        <SocialIcon url='https://www.github.com/zachmma99' target='_blank' className='social-icon' style={{ height: 35, width: 35 }} />
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/components/navlist/navlist.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
+import './navlist.scss';
+
+library.add(faCaretRight);
+
+export default class Navlist extends Component {
+
+    render() {
+        return(
+            <div className="navlist">
+                <div className="navlist-items">
+                    <div className="navlist-item">
+                        <h5>Home</h5>
+                        <FontAwesomeIcon icon="caret-right" className="arrow" />
+                    </div>
+                    <div className="navlist-item">
+                        <h5>About Me</h5>
+                        <FontAwesomeIcon icon="caret-right" className="arrow" />
+                    </div>
+                    <div className="navlist-item">
+                        <h5>Projects</h5>
+                        <FontAwesomeIcon icon="caret-right" className="arrow" />
+                    </div>
+                    <div className="navlist-item">
+                        <h5>Contact</h5>
+                        <FontAwesomeIcon icon="caret-right" className="arrow" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
