2dba7acfbeb762cfdd86733c34f034e4e7ecdcee
Zachary Johnson <johnsonz3@wit.edu>
2022-05-08 22:19:54 -0400

    Fixed dependencey issue, added navbar component and social icons as well as formatting for home page and building nav components for home page as well

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import Home from "./components/home/home";
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className='container'>
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path='/' exact element={ <Home /> } />   
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import Navbar from '../navbar/navbar';
+import Navlist from '../navlist/navlist';
 
 import './home.scss';
-
 
 export default class Home extends Component {
     
     render() {
         return(
             <div className='home container'>
-                <Navbar />
-                <h1>Zachary Johnson</h1>
-                <h3>
-                    Hello there, my name is Zachary Johnson. I'm a Boston-based Software Engineer.
-                </h3>
-
+                <Navlist />
+                <div className='home-content'>
+                    <h1>Zachary Johnson</h1>
+                    <h3>
+                        Hello there, my name is Zachary Johnson.
+                    </h3>
+                </div>
             </div>
         );
     }
